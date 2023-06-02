637b50c523be2afa3399501943953c4e7e242dea
Obscurely <adrian.obscurely@protonmail.com>
2023-06-02 18:53:16 +0300

    add some files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RustTemplate
+
 Rust Template
---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rustc
+    cmake
+    git
+    gcc
+    pkg-config
+  ];
+
+  RUST_BACKTRACE = 1;
+}
+
