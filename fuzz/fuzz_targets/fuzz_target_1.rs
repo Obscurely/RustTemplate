105edd005386930bc3271538d5ee1823485eb8f0
Obscurely <adrian.obscurely@protonmail.com>
2023-06-05 14:11:16 +0300

    format fuzz files

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,6 +1,6 @@
 #![no_main]
-use libfuzzer_sys::fuzz_target;
 use changeme_bin::add;
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let n1 = data.iter().map(|n| *n as u128).sum::<u128>() as u128;
