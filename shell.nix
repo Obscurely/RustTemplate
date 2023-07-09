c6afdcde61a1a936498fd401375d1eb0e621a46a
Obscurely <adrian.obscurely@protonmail.com>
2023-07-09 12:28:20 +0300

    complete shell.nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,19 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    llvmPackages_latest.llvm
+    llvmPackages_latest.bintools
+    zlib.out
+    xorriso
+    grub2
+    llvmPackages_latest.lld
     rustup
     rustc
     cmake
     git
     gcc
     pkg-config
+    python311
     python311Packages.pillow # this is for python repo script
   ];
 
