cb8dc37154197d631d153fce4c994853ff91e566
Obscurely <adrian.obscurely@protonmail.com>
2023-07-10 13:41:30 +0300

    swap iss to wix

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     #[cfg(windows)]
     {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("assets/images/logo.ico");
+        res.set_icon("assets\\images\\logo.ico");
         res.compile().unwrap();
     }
 }
